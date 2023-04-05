4a4892f39589ed70763bfca249af9f17a590836c
DUDA <mariabevenuto062@gmail.com>
2023-04-05 12:39:32 -0300

    feat: tweet dto

---
+++ src/main/java/com/tweteroo/api/dto/tweetDTO.java
@@ -0,0 +1,13 @@
+package com.tweteroo.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record tweetDTO(
+
+        @NotBlank
+        String username,
+
+        @NotBlank
+        String tweet) {
+
+}
